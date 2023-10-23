92510a3e1da503738060e34a527f1061344f46b0
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-10-23 15:57:55 -0300

    classesjs

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,3 +1,18 @@
+import { controls } from "./elements.js";
+import * as actions from './actions.js';
+
 export function registerControls(){
-  console.log('register');
+  controls.addEventListener('click', (event) =>{
+    const action = event.target.dataset.action;
+
+    // se o tipo de função que está presente na classe action não for do tipo função se retorna nada
+    if(typeof actions[action] != 'function'){
+      return
+    }else{
+      // caso for uma função execute a função que o usuario deseja
+      actions[action]();
+    }
+
+    
+  })
 }
---
+++ src/FocusTimer/timer.js
@@ -0,0 +1,17 @@
+import state from "./state.js"
+import * as el from './elements.js'
+
+export function updateDisplay(minutes, seconds){
+  //se caso caso a variavel minutes do cabeçalho da função for nulo, 
+  //o operador ?? está dizendo que ele vai preencher com o state.minutes 
+  minutes = minutes ?? state.minutes;
+
+  seconds = seconds ?? state.seconds;
+
+  el.minutes.textContent = String(minutes).padStart(2, '0' );
+  el.seconds.textContent = String(seconds).padStart(2, '0');
+}
+
+export function countDown(){
+  console.log('inciou');
+}
