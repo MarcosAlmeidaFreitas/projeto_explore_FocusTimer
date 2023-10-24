4b83c30aeafdb73ba699d276be875d06eafdfeca
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-10-24 12:33:56 -0300

    songs

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,16 +1,21 @@
 import state from "./state.js";
 import * as timer from './timer.js';
 import * as el from './elements.js';
+import * as sounds from './sounds.js';
+
 
 export function toggleRunning(){
   state.isRunning = document.documentElement.classList.toggle('running');
   timer.countDown();
+  sounds.buttonPressAudio.play();
 }
 
 export function reset(){
   state.isRunning = false;
   document.documentElement.classList.remove('running');
   timer.updateDisplay(0,0);
+  
+  sounds.buttonPressAudio.play();
 }
 
 export function set(){
